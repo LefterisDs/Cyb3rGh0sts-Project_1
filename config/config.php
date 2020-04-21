2f166a97f67bda944debad80f41dd87e0e49e8be
root <root@ip-172-30-0-52>
2020-04-21 18:55:06 +0000

    message

---
+++ config/config.php
@@ -0,0 +1,41 @@
+<?php
+/* ========================================================
+ * OpeneClass 2.2 configuration file
+ * Automatically created by install on 2020-04-21 13:32
+ * ======================================================== */
+
+$urlServer = "http://cybergh0sts.csec.chatzi.org/";
+$urlAppend = "";
+$webDir    = "/var/www/html/" ;
+
+$mysqlServer = "localhost";
+$mysqlUser = "root";
+$mysqlPassword = "J5yFWt6Dks";
+$mysqlMainDb = "eclass";
+$phpMyAdminURL = "../admin/mysql/";
+$phpSysInfoURL = "../admin/sysinfo/";
+$emailAdministrator = "webmaster@localhost";
+$administratorName = "Διαχειριστής";
+$administratorSurname = "Πλατφόρμας";
+$siteName = "Open eClass";
+
+$telephone = "+30 2xx xxxx xxx";
+$fax = "";
+$emailhelpdesk = "";
+
+$language = "greek";
+
+$Institution = "Ακαδημαϊκό Διαδίκτυο GUNet ";
+$InstitutionUrl = "http://www.gunet.gr/";
+$postaddress = "";
+
+$have_latex = FALSE;
+$close_user_registration = FALSE;
+
+$persoIsActive = TRUE;
+$durationAccount = "126144000";
+
+define("UTF8", true);
+
+
+$encryptedPasswd = true;
